627f5dad1c8f276dcc8fd14cc4c7f8dbca35b327
Dylan Alvarez <dylanalvarez1@ufl.edu>
2018-04-07 16:45:04 -0400

    Added link to google survey for feedback in results screen

--- States/js/win.js
+++ States/js/win.js
@@ -1,4 +1,34 @@
-console.log("winstate reached");
+class Link extends Phaser.Text {
+    constructor(game, x, y, text, url, style) {
+        super(game, x, y, text, style);
+        this.url = url;
+        this._oldFill = null;
+
+        //add custom objects to the game
+        this.game.add.existing(this);
+        //Activate inpute events
+        this.inputEnabled = true;
+        //Change hover cursor
+        this.input.useHandCursor = true;
+        //Listen to the events
+        this.events.onInputOver.add(this.onOver,this);
+        this.events.onInputOut.add(this.onOut, this);
+        this.events.onInputDown.add(this.onClick, this);
+    }
+
+    onOver() {
+        this._oldFill = this.fill;
+        this.fill = "blue";
+    }
+
+    onOut() {
+        this.fill = this._oldFill;
+    }
+
+    onClick() {
+        window.open(this.url, "_blank");
+    }
+}
 
 var winState={
   create: function(){
@@ -18,6 +48,7 @@ var winState={
     var statsLabel1 = game.add.text(80, 160, `Player 1 stats:` + '\n' + `Lives: ${Player1.lives}`); //Eventually integrate stats into player class, loop through and print out statistics for both players
     var statsLabel2 = game.add.text(game.world.width -240, 160, `Player 2 stats:` + '\n' + `Lives: ${Player2.lives}`);
     var startLabel=game.add.text(80,game.world.height-80,'Press "W" key to restart',{font: '25px Arial',fill:'#ffffff'});
+    let someLink = new Link(this.game, 80,game.world.height-120, "Click here to send feedback! Thanks for playing!", "https://goo.gl/forms/wA6NGUAJ4OiKhVC93", {font: '25px Arial',fill:'#ffffff'});
     var wkey= game.input.keyboard.addKey(Phaser.Keyboard.W);
     wkey.onDown.addOnce(this.start,this);
     if(game.device.android || game.device.iOS)
