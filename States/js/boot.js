1920b7a8d319ce5fc8c474fccb8df07846e324b2
Dylan Alvarez <dylanalvarez1@ufl.edu>
2018-04-08 19:49:44 -0400

    Update boot.js

--- States/js/boot.js
+++ States/js/boot.js
@@ -5,12 +5,8 @@ var bootState={
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		if(game.device.android || game.device.iOS)
 		{
-		  game.scale.fullScreenScaleMode = EXACT_FIT;
-		  game.scale.scaleMode = Phaser.ScaleManager.forceOrientation(forceLandscape);
-		  game.scale.setMaximum();
-		  game.scale.setScreenSize(true);
-		  game.scale.refresh();
 		  game.scale.startFullScreen();
+		  game.scale.fullScreenScaleMode = EXACT_FIT;
 		  console.log("This only logs for mobile, and will NEVER log for desktop")
 		}
 
