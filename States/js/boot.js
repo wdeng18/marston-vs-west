d52af692aff833c40dacb1a852862ba74b3d85e0
Dylan Alvarez <dylanalvarez1@ufl.edu>
2018-04-08 20:25:14 -0400

    testing mobile display

--- States/js/boot.js
+++ States/js/boot.js
@@ -5,8 +5,12 @@ var bootState={
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		if(game.device.android || game.device.iOS)
 		{
-		  game.scale.startFullScreen();
 		  game.scale.fullScreenScaleMode = EXACT_FIT;
+		  game.scale.scaleMode = Phaser.ScaleManager.forceOrientation(forceLandscape);
+		  game.scale.setMaximum();
+		  game.scale.setScreenSize(true);
+		  game.scale.refresh();
+		  //game.scale.startFullScreen();
 		  console.log("This only logs for mobile, and will NEVER log for desktop")
 		}
 
